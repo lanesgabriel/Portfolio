73b45fc957490049f823bf16087d3e61e29adf0c
lanesgabriel <gabrielchagaslanes21@gmail.com>
2023-01-02 11:32:48 -0300

    Updates

---
+++ precache-manifest.1a0300786b60f789ddab5600d626ca6b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21e3b8cb6297e9a22f7fd5023ca18920",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "59a793a796010c1b50e4",
+    "url": "/Portfolio/static/css/2.3eb2a443.chunk.css"
+  },
+  {
+    "revision": "fb9224ce24262f839eb0",
+    "url": "/Portfolio/static/css/main.3b3b9926.chunk.css"
+  },
+  {
+    "revision": "59a793a796010c1b50e4",
+    "url": "/Portfolio/static/js/2.7aebb008.chunk.js"
+  },
+  {
+    "revision": "ab4784922d7f45cc6cfd5a83588623bb",
+    "url": "/Portfolio/static/js/2.7aebb008.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb9224ce24262f839eb0",
+    "url": "/Portfolio/static/js/main.642d8d15.chunk.js"
+  },
+  {
+    "revision": "db1ea67b8a08076c9f67",
+    "url": "/Portfolio/static/js/runtime-main.0d26a884.js"
+  }
+]);
